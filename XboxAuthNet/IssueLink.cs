b72a6b409bbfa52ff0a060ebee17136ff7096381
ksi123456ab <ksi123456ab@naver.com>
2020-12-13 17:13:28 +0900

    handle exceptions

---
+++ XboxAuthNet/IssueLink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxAuthNet
+{
+    internal class IssueLink
+    {
+        public const string
+            UserTokenIssue = "https://bit.ly/xr-xbl-auth-user-token-issue",
+            CreateIssue = "https://bit.ly/xr-xbl-auth-create-issue",
+            UnauthorizedActivityError = "https://bit.ly/xr-xbl-auth-err-activity",
+            TwoFactorAuthenticationError = "https://bit.ly/xr-xbl-auth-err-2fa",
+
+            _dummy = "";
+    }
+}
---
+++ XboxAuthNet/XboxAuthException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxAuthNet
+{
+    public class XboxAuthException : Exception
+    {
+        public XboxAuthException(string message, string link)
+            : base(message)
+        {
+            base.HelpLink = link;
+        }
+
+        public XboxAuthException(string message, string link, Exception inner)
+            : base(message, inner)
+        {
+            base.HelpLink = link;
+        }
+
+        public XboxAuthException(string message, string link, HttpWebResponse res)
+            : base($"{message} {res.StatusCode}: {res.StatusDescription}")
+        {
+            base.HelpLink = link;
+            this.Response = res;
+        }
+
+        public HttpWebResponse Response { get; private set; }
+    }
+}
