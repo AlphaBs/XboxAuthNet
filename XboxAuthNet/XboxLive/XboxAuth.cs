8a32d68f469ca85e39ff350fc38861e47fc8080b
AlphaBs <ksi123456ab@naver.com>
2022-05-29 23:55:48 +0900

    2.0.0 beta1

--- XboxAuthNet/XboxLive/XboxAuth.cs
+++ XboxAuthNet/XboxLive/XboxAuth.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 //var rpsRes = exchanger.ExchangeRpsTicketForUserToken(AccessToken);
 //var xstsRes = exchanger.ExchangeTokensForXSTSIdentity(rpsRes.Token, null, null, XSTSRelyingParty, null);
@@ -9,90 +12,112 @@ namespace XboxAuthNet.XboxLive
 {
     public class XboxAuth
     {
+        private readonly HttpClient httpClient;
+
         public const string XboxScope = "service::user.auth.xboxlive.com::MBI_SSL";
 
         const string UserAuthenticate = "https://user.auth.xboxlive.com/user/authenticate";
         const string XstsAuthorize = "https://xsts.auth.xboxlive.com/xsts/authorize";
         const string DefaultRelyingParty = "http://xboxlive.com";
 
-        public XboxAuthResponse ExchangeRpsTicketForUserToken(string rps)
+        public XboxAuth(HttpClient client)
+        {
+            this.httpClient = client;
+        }
+
+        private async Task<XboxAuthResponse> xboxRequest(HttpRequestMessage req, string contractVersion)
+        {
+            req.Headers.Add("User-Agent", HttpUtil.UserAgent);
+            req.Headers.Add("Accept", "application/json");
+            req.Headers.Add("Accept-Encoding", "gzip");
+            req.Headers.Add("Accept-Language", "en-US");
+            req.Headers.Add("x-xbl-contract-version", contractVersion);
+            req.Headers.TransferEncodingChunked = false;
+
+            var res = await httpClient.SendAsync(req)
+                .ConfigureAwait(false);
+
+            res.EnsureSuccessStatusCode();
+            var resBody = await res.Content.ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            return parseAuthResponse(resBody);
+        }
+
+        private XboxAuthResponse parseAuthResponse(string json)
+        {
+            using var jsonDocument = JsonDocument.Parse(json);
+            var root = jsonDocument.RootElement;
+            var xboxResponse = root.Deserialize<XboxAuthResponse>();
+
+            if (xboxResponse != null &&
+                root.TryGetProperty("DisplayClaims", out var displayClaims) &&
+                displayClaims.TryGetProperty("xui", out var xui))
+            {
+                var firstXui = xui.EnumerateArray().First();
+
+                if (firstXui.TryGetProperty("xid", out var xid))
+                    xboxResponse.UserXUID = xid.GetString();
+
+                if (firstXui.TryGetProperty("uhs", out var uhs))
+                    xboxResponse.UserHash = uhs.GetString();
+            }
+
+            return xboxResponse ?? new XboxAuthResponse(false);
+        }
+
+        public async Task<XboxAuthResponse> ExchangeRpsTicketForUserToken(string rps)
         {
             try
             {
-                var req = HttpUtil.CreateDefaultRequest(UserAuthenticate);
-                req.Method = "POST";
-                req.ContentType = "application/json";
-                req.Accept = "application/json";
-                req.Headers["x-xbl-contract-version"] = "0";
-
-                var reqBody = new JObject()
+                var res = await xboxRequest(new HttpRequestMessage
                 {
-                    { "RelyingParty", "http://auth.xboxlive.com" },
-                    { "TokenType", "JWT" },
-                    { "Properties", new JObject()
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(UserAuthenticate),
+                    Content = createJsonContent(new
+                    {
+                        RelyingParty = "http://auth.xboxlive.com",
+                        TokenType = "JWT",
+                        Properties = new
                         {
-                            { "AuthMethod", "RPS" },
-                            { "SiteName", "user.auth.xboxlive.com" },
-                            { "RpsTicket", rps }
+                            AuthMethod = "RPS",
+                            SiteName = "user.auth.xboxlive.com",
+                            RpsTicket = rps
                         }
-                    }
-                };
-                HttpUtil.WriteRequest(req, reqBody.ToString());
-
-                var res = req.GetResponseNoException();
-                var body = HttpUtil.ReadResponse(res);
-
-                return parseAuthResponse(body);
-            }
-            catch (XboxAuthException)
-            {
-                throw;
+                    })
+                }, contractVersion: "0").ConfigureAwait(false);
+                return res;
             }
             catch (Exception ex)
             {
-                throw new XboxAuthException("Failed to " + nameof(ExchangeRpsTicketForUserToken),  null, ex);
+                throw new XboxAuthException("Failed to " + nameof(ExchangeRpsTicketForUserToken), null, ex);
             }
         }
 
-        public XboxAuthResponse ExchangeTokensForXstsIdentity(string userToken, string? deviceToken, string? titleToken,
+        public async Task<XboxAuthResponse> ExchangeTokensForXstsIdentity(string userToken, string? deviceToken, string? titleToken,
             string? xstsRelyingParty, string[]? optionalDisplayClaims)
         {
             try
             {
-                var req = HttpUtil.CreateDefaultRequest(XstsAuthorize);
-                req.Method = "POST";
-                req.ContentType = "application/json";
-                req.Accept = "application/json";
-                req.Headers["x-xbl-contract-version"] = "1";
-
-                var reqBody = JObject.FromObject(new
+                var res = await xboxRequest(new HttpRequestMessage
                 {
-                    RelyingParty = xstsRelyingParty ?? DefaultRelyingParty,
-                    TokenType = "JWT",
-                    Properties = new
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(XstsAuthorize),
+                    Content = createJsonContent(new
                     {
-                        UserTokens = nullHandle(userToken),
-                        DeviceToken = deviceToken,
-                        TitleToken = titleToken,
-                        OptionalDisplayClaims = nullHandle(optionalDisplayClaims),
-                        SandboxId = "RETAIL"
-                    }
-                },
-                new JsonSerializer
-                {
-                    NullValueHandling = NullValueHandling.Ignore
-                });
-
-                HttpUtil.WriteRequest(req, reqBody.ToString());
-
-                var res = req.GetResponseNoException();
-                var body = HttpUtil.ReadResponse(res);
-
-                return parseAuthResponse(body);
-            }
-            catch (XboxAuthException)
-            {
-                throw;
+                        RelyingParty = xstsRelyingParty ?? DefaultRelyingParty,
+                        TokenType = "JWT",
+                        Properties = new
+                        {
+                            UserTokens = new string[] { userToken },
+                            DeviceToken = deviceToken,
+                            TitleToken = titleToken,
+                            OptionalDisplayClaims = optionalDisplayClaims,
+                            SandboxId = "RETAIL"
+                        }
+                    }),
+                }, contractVersion: "1").ConfigureAwait(false);
+                return res;
             }
             catch (Exception ex)
             {
@@ -100,33 +125,15 @@ namespace XboxAuthNet.XboxLive
             }
         }
 
-        private XboxAuthResponse parseAuthResponse(string json)
+        private JsonContent createJsonContent<T>(T obj)
         {
-            var job = JObject.Parse(json);
-            var authres = job.ToObject<XboxAuthResponse>();
-            if (authres == null)
-                return new XboxAuthResponse();
-
-            authres.UserXUID = job["DisplayClaims"]?["xui"]?[0]?["xid"]?.ToString();
-            authres.UserHash = job["DisplayClaims"]?["xui"]?[0]?["uhs"]?.ToString();
-
-            return authres;
-        }
-
-        private JArray? nullHandle(string str)
-        {
-            if (string.IsNullOrEmpty(str))
-                return null;
-            else
-                return new JArray(str);
-        }
-
-        private JArray? nullHandle(object[]? objs)
-        {
-            if (objs == null || objs.Length == 0)
-                return null;
-            else
-                return new JArray(objs);
+            return JsonContent.Create(obj,
+                mediaType: new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"),
+                options: new JsonSerializerOptions
+                {
+                    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                    PropertyNamingPolicy = null
+                });
         }
     }
 }
