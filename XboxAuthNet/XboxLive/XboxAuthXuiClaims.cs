35c5c1b030a76144733517ff1b2b87c3ff60f111
AlphaBs <ksi123456ab@naver.com>
2022-07-29 22:12:20 +0900

    add XboxAuthXuiClaims

---
+++ XboxAuthNet/XboxLive/XboxAuthXuiClaims.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace XboxAuthNet.XboxLive
+{
+    // https://github.com/OpenXbox/xbox-webapi-csharp/blob/master/XboxWebApi/Authentication/Model/XboxUserInformation.cs
+    public class XboxAuthXuiClaims
+    {
+        [JsonPropertyName("gtg")]
+        public string? Gamertag { get; set; }
+
+        [JsonPropertyName("xid")]
+        public string? XboxUserId { get; set; }
+
+        [JsonPropertyName("uhs")]
+        public string? UserHash { get; set; }
+
+        [JsonPropertyName("agg")]
+        public string? AgeGroup { get; set; }
+
+        [JsonPropertyName("usr")]
+        public string? UserSettingsRestrictions { get; set; }
+
+        [JsonPropertyName("utr")]
+        public string? UserTitleRestrictions { get; set; }
+
+        [JsonPropertyName("prv")]
+        public string? Privileges { get; set; }
+    }
+}
