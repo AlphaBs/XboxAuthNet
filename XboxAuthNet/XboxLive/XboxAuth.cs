336c9e6af94953c85912b0ed90c92bef40253d30
권세인 <ksi123456ab@naver.com>
2023-03-18 10:16:50 +0000

    fix wrong namespace XboxAuthNet.XboxLive.Entity.XboxRequest -> XboxAuthNet.XboxLive.XboxRequest

--- XboxAuthNet/XboxLive/XboxAuth.cs
+++ XboxAuthNet/XboxLive/XboxAuth.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
-using XboxAuthNet.XboxLive.Entity;
 using XboxAuthNet.XboxLive.Models;
 
 //var rpsRes = exchanger.ExchangeRpsTicketForUserToken(AccessToken);
--- XboxAuthNet/XboxLive/XboxRequest.cs
+++ XboxAuthNet/XboxLive/XboxRequest.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace XboxAuthNet.XboxLive.Entity
+namespace XboxAuthNet.XboxLive
 {
     public class XboxRequest
     {
